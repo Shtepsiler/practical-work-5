33e41730a554f8627761418e9673e525cee8b7eb
Shtepsiler <100515332+Shtepsiler@users.noreply.github.com>
2022-10-16 20:26:44 +0300

    зміни

--- ConsoleApp1/ConsoleApp5/Dough.cs
+++ ConsoleApp1/ConsoleApp5/Dough.cs
@@ -8,21 +8,62 @@ namespace ConsoleApp5
 {
     internal class Dough
     {
-        double white = 1.5;
-        public double White { get; set; } 
-        double wholegrain = 1.0;
+        private double white = 1.5;
+        public double White { get; set; }
+
+        private double wholegrain = 1.0;
         public double Wholegrain { get; set; }
-        double crispy = 0.9;
+
+        private double crispy = 0.9;
         public double Crispy { get; set; }
-        double chewy = 1.1;
+
+        private double chewy = 1.1;
         public double Chewy { get; set; }
-        double homemade = 1.0;
+
+        private double homemade = 1.0;
         public double Homemade { get; set; }
 
         
         public double Calories(double waihgt )
         {
             double calories=0;
+            double fl;
+            double tec;
+
+            Console.WriteLine("Write flour type");
+            string flour = Console.ReadLine();
+            if( flour == "white" || flour =="White" )
+            {
+                fl = White;
+
+            }else
+                if (flour == "wholegrain" || flour == "Wholegrain")
+            {
+                fl = Wholegrain;
+            }else
+                Console.WriteLine("there is no one");
+
+
+
+            Console.WriteLine("Write flour type");
+            string techic = Console.ReadLine();
+            if (techic == "white" || techic == "White")
+            {
+                tec = White;
+
+            }
+            else
+                if (techic == "wholegrain" || techic == "Wholegrain")
+            {
+                tec = Wholegrain;
+            }
+            else
+                if (techic == "wholegrain" || techic == "Wholegrain")
+            {
+                tec = Wholegrain;
+            }else
+            Console.WriteLine("there is no one");
+
 
 
             return calories;
